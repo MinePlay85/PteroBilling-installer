d10a1d7ea8a78ff1f51fd7bc88a5dd77c28adae5
MinePlay85 <85mathysmathys@gmail.com>
2021-05-05 14:09:44 +0200

    Wrong branch

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ set -e
 S_VERSION="0.5"
 PUBLIC_REPO="https://github.com/MinePlay85/PteroBilling-installer" #Mark repo of the installer
 SPONSOR="https://paypal.me/alaisterleung" 
-INSTALL_LINK="https://raw.githubusercontent.com/MinePlay85/pterobilling-installer/master/install-pterobilling.sh"
+INSTALL_LINK="https://raw.githubusercontent.com/MinePlay85/pterobilling-installer/dev/install-pterobilling.sh"
 
 # exit with error if user is not root
 if [[ $EUID -ne 0 ]]; then
