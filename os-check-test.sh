ca55b8e46355cab2930417906467f420342bc71c
MinePlay85 <85mathysmathys@gmail.com>
2021-05-07 18:47:07 +0200

    Adding a OS test file

---
+++ os-check-test.sh
@@ -0,0 +1,21 @@
+set -e
+
+###
+# Test OS version checker
+###
+
+OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
+
+if [ -z "$OS" ]; then
+    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+fi
+
+if [ -z "$VERSION" ]; then
+    VERSION=$(awk '{print $3}' /etc/*-release)
+fi
+
+echo $OS
+echo $ARCH
+echo $VERSION
